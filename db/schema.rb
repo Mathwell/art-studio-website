5d94bbf8704d187bca5dce388452563be23a5396
Mathwell <eageeva@hotmail.com>
2018-12-22 13:00:22 -0800

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_07_174625) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "artworks", force: :cascade do |t|
     t.string "author"
     t.string "description"
